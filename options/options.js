9b825548369efd8763855c5c890cc32d778311c1
Crowdbotics <team@crowdbotics.com>
2023-09-22 18:23:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ancient-credit-43519",
+  url: "https://ancient-credit-43519.botics.co",
+  api: "https://ancient-credit-43519.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
